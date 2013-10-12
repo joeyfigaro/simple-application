954e6efed3a1c50a86e87e146ded2593b0346791
Joey Figaro <joey@joeyfigaro.com>
2013-10-11 20:50:32 -0400

    added head.js snippet

---
+++ selectbox-url-linking.js
@@ -0,0 +1,33 @@
+// Snippet from the head.js used for the new VCU site
+
+head.ready('selectBox', function() {
+  console.log('head.js: (selectBox) initialized');
+
+  $('#search select[name="also-try"]').selectBoxIt({
+    defaultText: 'Also try'
+  });
+
+  $('#search select[name="also-try"]').bind({
+    "option-click": function(e, obj) {
+      navigateTo(obj.dropdownOption[0].textContent);
+    }
+  });
+
+  function navigateTo(url) {
+    var url = url.toLowerCase();
+    var destination;
+    var destination_array = [];
+
+    destination_array = {
+      pubmed: 'http://www.ncbi.nlm.nih.gov/pubmed?otool=vculib',
+      database: 'https://apps.library.vcu.edu/dblist',
+      libguides: 'http://guides.library.vcu.edu/home',
+      advanced: 'http://google.com',
+      help: 'http://google.com',
+    }
+
+    destination = destination_array[url];
+
+    window.location.href = destination;
+  }
+});
